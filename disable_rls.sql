236e611d7ea094b3059f5e683ad3bcd2a89669b6
Marco Semino <paco.semino@gmail.com>
2025-12-09 11:40:48 -0300

    Elimine Todos los .sql que no debian de estar

--- disable_rls.sql
+++
@@ -1,7 +0,0 @@
--- Desactivar temporalmente RLS para la tabla profiles
-ALTER TABLE public.profiles DISABLE ROW LEVEL SECURITY;
-
--- Crear una política que permita acceso público a la tabla profiles
-CREATE POLICY "Acceso público temporal a profiles" 
-  ON public.profiles FOR ALL 
-  USING (true);
