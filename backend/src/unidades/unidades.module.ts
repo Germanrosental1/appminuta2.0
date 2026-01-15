5dab6513580f81cf893ec98f88ab1b6407893cce
paco semino <pacosemino@MacBook-Pro-de-paco.local>
2026-01-15 09:51:06 -0300

    fix map import masivo

--- backend/src/unidades/unidades.module.ts
+++ backend/src/unidades/unidades.module.ts
@@ -4,9 +4,11 @@ import { UnidadesService } from './unidades.service';
 import { UnidadesQueryService } from './unidades-query.service';
 import { UnidadesImportService } from './unidades-import.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { UsuariosRolesModule } from '../usuarios-roles/usuarios-roles.module';
+import { LoggerModule } from '../logger/logger.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UsuariosRolesModule, LoggerModule],
   controllers: [UnidadesController],
   providers: [UnidadesService, UnidadesQueryService, UnidadesImportService],
   exports: [UnidadesService, UnidadesQueryService]
